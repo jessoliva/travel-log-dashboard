e03304f1910f7eb2b341bc0dec9e9ef2741993a2
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-03-31 16:07:42 -0500

    created files for each route, set up index for api routes

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const sess = {
+    secret: '42',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
