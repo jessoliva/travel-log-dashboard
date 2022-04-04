536394451a06a52cc9d94f2973fc6feea295623f
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-04-03 20:57:05 -0500

    fixed server.js file

--- server.js
+++ server.js
@@ -1,49 +1,35 @@
-const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const path = require("path");
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-// handlebar dependencies
+// handlebars dependencies
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+    secret: '42',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
 };
-
-app.use(session(sess));
-
-// const helpers = require('./utils/helpers');
-
-const hbs = exphbs.create({});
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(session(sess));
-
+// handlebars engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(require('./controllers/'));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
