b950659d3bb325feff1a74208f8a8b7a1ade66fe
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-04-04 20:14:56 -0500

    added seeds and photos files

---
+++ seeds/user-seeds.js
@@ -0,0 +1,29 @@
+const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
+
+const userdata = [
+  {
+    username: 'jessoli',
+    password:'password1122',
+    email:'jessoli@ut.com',
+  },
+  {
+    username:'stevietru',
+    password:'password1122',
+    email:'stevietru@ut.com'
+  },
+  {
+    username:'sheroobha',
+    password:'password1122',
+    email:'sheroo@ut.com'
+  },
+  {
+    username:'edmerval',
+    password:'password1122',
+    email:'edmerval@ut.com'
+  }
+];
+
+const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
+
+module.exports = seedUsers;
