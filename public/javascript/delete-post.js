b1ef4b212ba7899e19b3aa859b9aefea991adbb3
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-04-10 13:26:17 -0500

    final merge

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,20 +1,57 @@
-async function deleteFormHandler(event) {
-    event.preventDefault();
+// async function deleteFormHandler(event) {
+//     event.preventDefault();
   
-    const id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
+//     const id = window.location.toString().split('/')[
+//       window.location.toString().split('/').length - 1
+//     ];
 
-    const response = await fetch(`/api/posts/${id}`, {
-      method: 'DELETE'
-    });
+//     const response = await fetch(`/api/posts/${id}`, {
+//       method: 'DELETE'
+//     });
   
-    if (response.ok) {
-      document.location.replace('/my-posts');
-    } else {
-      alert(response.statusText);
-    }
+//     if (response.ok) {
+//       document.location.replace('/my-posts');
+//     } else {
+//       alert(response.statusText);
+//     }
+// }
+  
+// document.querySelector('#delete-post-btn').addEventListener('click', deleteFormHandler);
+  
+async function deleteFormHandler(event) {
+  event.preventDefault();
+
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'DELETE'
+  });
+
+  if (response.ok) {
+    document.location.replace('/my-posts');
+  } else {
+    alert(response.statusText);
+  }
+};
+document.querySelector('#delete-post').addEventListener('click', deleteFormHandler);
+
+// modal element
+const modalEl = document.querySelector('#generate-modal');
+
+function displayModal() {
+// display modal
+modalEl.classList.add("show");
+modalEl.classList.remove("hide");
+};
+document.querySelector('#delete-post-btn').addEventListener('click', displayModal);
+
+function closeModal(event) {
+event.preventDefault();
+
+// close modal
+modalEl.classList.add("hide");
+modalEl.classList.remove("show");
 }
-  
-document.querySelector('#delete-post-btn').addEventListener('click', deleteFormHandler);
-  
+document.querySelector('#close-modal').addEventListener('click', closeModal);
